0015ca5c33c35b32812d9b0dc224dfd2438fa607
Mark Wetzel <mark@markwetzel.com>
2015-03-20 12:03:27 -0400

    Changed root to albums#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,16 @@
 Rails.application.routes.draw do
 
+  root 'albums#index'
 
+  resources :albums
+  resources :songs
 
-  resources :albums do 
-    resources :songs
+  resources :albums, except: [:new], path: '' do 
+    resources :songs, except: [:index], path: ''
   end
-
-  root 'static_pages#home'
 
   get '/about', to: 'static_pages#about', as: 'about'
   get '/contact', to: 'static_pages#contact', as: 'contact'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
