d5e4f5273c4be5f657f2c2fa2e6c536c7bd2d512
Mark Wetzel <mark@markwetzel.com>
2015-03-22 10:25:30 -0400

    Cleaned up new/edit Song.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -36,7 +36,8 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+      format.html { redirect_to album_song_path(@comment.song.album, @comment.song),
+        notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
