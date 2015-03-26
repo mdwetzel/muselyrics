6045c3c25b40f3c7a3244e0eaa24969378b2d148
Mark Wetzel <mark@markwetzel.com>
2015-03-26 16:04:21 -0400

    Added username to User.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from CanCan::AccessDenied do |exception|
   	if user_signed_in?
   		redirect_to root_url, alert: exception.message
   	else
   		redirect_to login_path 
-	end
+	  end
   end
+  
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,28 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   has_many :comments, dependent: :destroy
+
+  validates :username,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false,
+    },
+    :length => { minimum: 3, maximum: 16 }
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
