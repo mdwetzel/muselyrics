405da27d80c346f1f3e440f8bff63b3ff4fb6262
Mark Wetzel <mark@markwetzel.com>
2015-04-08 17:35:17 -0400

    Fixed madness.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,13 @@
 class MessagesController < ApplicationController
 
-  before_action :set_madness, only: [:index, :create]
+  include SongsHelper 
 
 	def index
 		@message = Message.new
 	end
 
 	def create
-		@message = Message.new(message_params)
+	  @message = Message.new(message_params)
     @message.ip = request.remote_ip
     @message.sent_at = Time.now
 
@@ -20,10 +20,6 @@ class MessagesController < ApplicationController
 	end
 
 		private
-
-    def set_madness
-      @madness = Song.find_by(title: "Madness")
-    end
 
 		def message_params
 			params.require(:message).permit(:body, :name, :email)
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,11 @@
 module SongsHelper
+	def madness_link
+		madness = Song.madness
+
+		if madness
+			link_to madness.title.downcase, [madness.album, madness]
+		else
+			"madness"
+		end
+	end
 end
