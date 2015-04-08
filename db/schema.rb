050fa75238f01800ad716ccad1c513d2e9101631
Mark Wetzel <mark@markwetzel.com>
2015-04-08 18:43:08 -0400

    Added avatars to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408215638) do
+ActiveRecord::Schema.define(version: 20150408223659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20150408215638) do
     t.boolean  "member",                 default: true
     t.string   "username"
     t.boolean  "moderator",              default: false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
