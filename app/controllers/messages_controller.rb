1509e375dfff3bb121b3ed1dddc5a06d7c9fef9c
Mark Wetzel <mark@markwetzel.com>
2015-04-08 13:49:36 -0400

    Updated contact page.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,15 @@
 class MessagesController < ApplicationController
+
+  before_action :set_madness, only: [:index, :create]
+
 	def index
 		@message = Message.new
 	end
 
 	def create
 		@message = Message.new(message_params)
+    @message.ip = request.remote_ip
+    @message.sent_at = Time.now
 
     if(@message.valid?)
   		ContactMailer.new_message(@message).deliver_now
@@ -15,6 +20,10 @@ class MessagesController < ApplicationController
 	end
 
 		private
+
+    def set_madness
+      @madness = Song.find_by(title: "Madness")
+    end
 
 		def message_params
 			params.require(:message).permit(:body, :name, :email)
